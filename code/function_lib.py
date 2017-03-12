4b6121cdce17b7462a06d5569fb8cfb8d4e14dfd
chili <liheng.lc.chen@gmail.com>
2017-03-12 15:08:07 +0100

    chili: finish image processing

--- code/function_lib.py
+++ code/function_lib.py
@@ -7,6 +7,7 @@ import matplotlib.image as mpimg
 from sklearn.svm import SVC
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
+from scipy.ndimage.measurements import label
 
 
 def convert_color(img, conv='RGB2YCrCb'):
@@ -75,6 +76,40 @@ def draw_boxes(img, bboxes, color=(0, 0, 255), thick=6):
         cv2.rectangle(imcopy, bbox[0], bbox[1], color, thick)
     # Return the image copy with boxes drawn
     return imcopy
+
+
+def add_heat(heatmap, bbox_list):
+    # Iterate through list of bboxes
+    for box in bbox_list:
+        # Add += 1 for all pixels inside each bbox
+        # Assuming each "box" takes the form ((x1, y1), (x2, y2))
+        heatmap[box[0][1]:box[1][1], box[0][0]:box[1][0]] += 1
+
+    # Return updated heatmap
+    return heatmap  # Iterate through list of bboxes
+
+
+def apply_threshold(heatmap, threshold):
+    # Zero out pixels below the threshold
+    heatmap[heatmap <= threshold] = 0
+    # Return thresholded map
+    return heatmap
+
+
+def draw_labeled_bboxes(img, labels):
+    # Iterate through all detected cars
+    for car_number in range(1, labels[1] + 1):
+        # Find pixels with each car_number label value
+        nonzero = (labels[0] == car_number).nonzero()
+        # Identify x and y values of those pixels
+        nonzeroy = np.array(nonzero[0])
+        nonzerox = np.array(nonzero[1])
+        # Define a bounding box based on min/max x and y
+        bbox = ((np.min(nonzerox), np.min(nonzeroy)), (np.max(nonzerox), np.max(nonzeroy)))
+        # Draw the box on the image
+        cv2.rectangle(img, bbox[0], bbox[1], (0, 255, 255), 6)
+    # Return the image
+    return img
 
 
 def slide_window(img, x_start_stop=[None, None], y_start_stop=[None, None],
@@ -172,8 +207,9 @@ def extract_features(imgs, color_space='RGB', spatial_size=(32, 32),
     return features
 
 
-def train_classifier(folder_cars, folder_non_cars, force_gen_feature=False, colorspace='RGB',
-                     orient=24, pix_per_cell=8, cell_per_block=1,
+def train_classifier(folder_cars, folder_non_cars, svc_file, features_file,
+                     force_gen_feature=False, force_gen_svc=False,
+                     colorspace='RGB', orient=24, pix_per_cell=8, cell_per_block=1,
                      hog_channel='ALL', spatial_size=(32, 32), hist_bins=32):
 
     # get all file names of non car images
@@ -198,13 +234,15 @@ def train_classifier(folder_cars, folder_non_cars, force_gen_feature=False, colo
 
     # extract features
     cwd = os.getcwd()
-    features_file = 'features.pickle'
+
     if features_file in os.listdir(cwd) or force_gen_feature:
         print('Features already generated, load features...')
         with open(features_file, 'rb') as f:
             features = pickle.load(f)
             car_features = features['car_features']
             notcar_features = features['notcar_features']
+        svc_up2date = True
+
     else:
         t = time.time()
         car_features = extract_features(cars, color_space=colorspace, orient=orient,
@@ -218,6 +256,7 @@ def train_classifier(folder_cars, folder_non_cars, force_gen_feature=False, colo
             pickle.dump(features, f)
         t2 = time.time()
         print(round(t2 - t, 2), 'Seconds to extract HOG features...')
+        svc_up2date = False
 
     # create an array stack of feature vectors
     X = np.vstack((car_features, notcar_features)).astype(np.float64)
@@ -234,24 +273,161 @@ def train_classifier(folder_cars, folder_non_cars, force_gen_feature=False, colo
     X_train, X_test, y_train, y_test = train_test_split(
         scaled_X, y, test_size=0.2, random_state=rand_state)
 
-    # train svm
-    print('Using:', orient, 'orientations', pix_per_cell,
-          'pixels per cell and', cell_per_block, 'cells per block')
-    print('Feature vector length:', len(X_train[0]))
+    if force_gen_svc or not svc_up2date or svc_file not in os.listdir(cwd):
+        # train svm
+        print('Using:', orient, 'orientations', pix_per_cell,
+              'pixels per cell and', cell_per_block, 'cells per block')
+        print('Feature vector length:', len(X_train[0]))
 
-    # svc = LinearSVC()
-    svc = SVC(kernel='linear')
-    # Check the training time for the SVC
-    t = time.time()
-    svc.fit(X_train, y_train)
-    t2 = time.time()
-    print(round(t2 - t, 2), 'Seconds to train SVC...')
-    # Check the score of the SVC
-    print('Test Accuracy of SVC = ', round(svc.score(X_test, y_test), 4))
-    # Check the prediction time for a single sample
-    t = time.time()
-    n_predict = 10
-    print('My SVC predicts: ', svc.predict(X_test[0:n_predict]))
-    print('For these', n_predict, 'labels: ', y_test[0:n_predict])
-    t2 = time.time()
-    print(round(t2 - t, 5), 'Seconds to predict', n_predict, 'labels with SVC')
+        # define svm
+        svc = SVC(kernel='linear')
+
+        t = time.time()
+        svc.fit(X_train, y_train)
+        t2 = time.time()
+        print(round(t2 - t, 2), 'Seconds to train SVC...')
+        # Check the score of the SVC
+        print('Test Accuracy of SVC = ', round(svc.score(X_test, y_test), 4))
+
+        # save all data
+        svc_data = {'svc': svc, 'scaler': X_scaler, 'orient': orient, 'pix_per_cell': pix_per_cell,
+                    'cell_per_block': cell_per_block, 'spatial_size': spatial_size, 'hist_bins': hist_bins}
+
+        with open(svc_file, 'wb') as f:
+            pickle.dump(svc_data, f)
+
+
+def find_cars(img, ystart, ystop, scale, svc, X_scaler, orient, pix_per_cell, cell_per_block, spatial_size, hist_bins,
+              spatial_feat=False, hist_feat=False):
+    draw_img = np.copy(img)
+    img = img.astype(np.float32) / 255
+
+    img_tosearch = img[ystart:ystop, :, :]
+    ctrans_tosearch = convert_color(img_tosearch, conv='RGB2YCrCb')
+    if scale != 1:
+        imshape = ctrans_tosearch.shape
+        ctrans_tosearch = cv2.resize(ctrans_tosearch, (np.int(imshape[1] / scale), np.int(imshape[0] / scale)))
+
+    ch1 = ctrans_tosearch[:, :, 0]
+    ch2 = ctrans_tosearch[:, :, 1]
+    ch3 = ctrans_tosearch[:, :, 2]
+
+    # Define blocks and steps as above
+    nxblocks = (ch1.shape[1] // pix_per_cell) - 1
+    nyblocks = (ch1.shape[0] // pix_per_cell) - 1
+    nfeat_per_block = orient * cell_per_block ** 2
+    # 64 was the orginal sampling rate, with 8 cells and 8 pix per cell
+    window = 64
+    nblocks_per_window = (window // pix_per_cell)
+    cells_per_step = 2  # Instead of overlap, define how many cells to step
+    nxsteps = (nxblocks - nblocks_per_window) // cells_per_step
+    nysteps = (nyblocks - nblocks_per_window) // cells_per_step
+
+    # Compute individual channel HOG features for the entire image
+    hog1 = get_hog_features(ch1, orient, pix_per_cell, cell_per_block, feature_vec=False)
+    hog2 = get_hog_features(ch2, orient, pix_per_cell, cell_per_block, feature_vec=False)
+    hog3 = get_hog_features(ch3, orient, pix_per_cell, cell_per_block, feature_vec=False)
+
+    vehicles_boxes = []
+
+    for xb in range(nxsteps):
+        for yb in range(nysteps):
+            ypos = yb * cells_per_step
+            xpos = xb * cells_per_step
+            # Extract HOG for this patch
+            hog_feat1 = hog1[ypos:ypos + nblocks_per_window, xpos:xpos + nblocks_per_window].ravel()
+            hog_feat2 = hog2[ypos:ypos + nblocks_per_window, xpos:xpos + nblocks_per_window].ravel()
+            hog_feat3 = hog3[ypos:ypos + nblocks_per_window, xpos:xpos + nblocks_per_window].ravel()
+            hog_features = np.hstack((hog_feat1, hog_feat2, hog_feat3))
+
+            xleft = xpos * pix_per_cell
+            ytop = ypos * pix_per_cell
+
+            # Extract the image patch
+            subimg = cv2.resize(ctrans_tosearch[ytop:ytop + window, xleft:xleft + window], (64, 64))
+
+            features = []
+            # Get color features
+            if spatial_feat:
+                spatial_features = bin_spatial(subimg, size=spatial_size)
+                features.append(spatial_features)
+            if hist_feat:
+                hist_features = color_hist(subimg, nbins=hist_bins)
+                features.append(hist_features)
+
+            features.append(hog_features)
+
+            # Scale features and make a prediction
+            test_features = X_scaler.transform(features)
+            # test_features = X_scaler.transform(np.hstack((shape_feat, hist_feat)).reshape(1, -1))
+            test_prediction = svc.predict(test_features)
+
+
+            if test_prediction == 1:
+                xbox_left = np.int(xleft * scale)
+                ytop_draw = np.int(ytop * scale)
+                win_draw = np.int(window * scale)
+                #cv2.rectangle(draw_img, (xbox_left, ytop_draw + ystart),
+                #              (xbox_left + win_draw, ytop_draw + win_draw + ystart), (0, 0, 255), 6)
+                vehicles_boxes.append(((xbox_left, ytop_draw + ystart),
+                                       (xbox_left + win_draw, ytop_draw + win_draw + ystart)))
+
+    return vehicles_boxes
+
+
+def generate_heatmap(img, vehicles_boxes):
+    # Read in image similar to one shown above
+    heat = np.zeros_like(img[:, :, 0]).astype(np.float)
+
+    # Add heat to each box in box list
+    heat = add_heat(heat, vehicles_boxes)
+
+    # Apply threshold to help remove false positives
+    heat = apply_threshold(heat, 3)
+
+    # Visualize the heatmap when displaying
+    heatmap = np.clip(heat, 0, 255)
+
+    # Find final boxes from heatmap using label function
+    labels = label(heatmap)
+    draw_img = draw_labeled_bboxes(np.copy(img), labels)
+
+    return draw_img, heatmap
+
+
+def process_image(img):
+    # define multiple search areas
+    ystarts = [400, 400, 400]
+    ystops = [656, 656, 528]
+    scales = [1.5, 1, 0.75]
+
+    # get classifier
+    svc_file = 'svc.pickle'
+    with open(svc_file, "rb") as f:
+        svc_data = pickle.load(f)
+    svc = svc_data["svc"]
+    X_scaler = svc_data["scaler"]
+
+    # obtain parameters of classifier
+    orient = svc_data["orient"]
+    pix_per_cell = svc_data["pix_per_cell"]
+    cell_per_block = svc_data["cell_per_block"]
+    spatial_size = svc_data["spatial_size"]
+    hist_bins = svc_data["hist_bins"]
+
+    # find cars on image, returns list of position boxes
+    vehicles_boxes = []
+    for ystart, ystop, scale in zip(ystarts, ystops, scales):
+        v_boxes = find_cars(img, ystart, ystop, scale, svc, X_scaler, orient,
+                            pix_per_cell, cell_per_block, spatial_size, hist_bins)
+        if len(v_boxes) > 0:
+            for box in v_boxes:
+                vehicles_boxes.append(box)
+
+    #draw_img = draw_boxes(img, vehicles_boxes)
+
+    # generate heat maps
+    draw_img, heat_map = generate_heatmap(img, vehicles_boxes)
+
+    return heat_map
+
--- code/main.py
+++ code/main.py
@@ -3,4 +3,63 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pickle
 import cv2
+import os
 from function_lib import *
+
+# parameter for training
+folder_cars = '../vehicles/'
+folder_non_cars = '../non-vehicles/'
+svc_file = 'svc.pickle'
+features_file = 'features.pickle'
+color_space = 'YCrCb'  # Can be RGB, HSV, LUV, HLS, YUV, YCrCb
+orient = 24
+pix_per_cell = 8
+cell_per_block = 1
+hog_channel = 'ALL'
+spatial_size = (40, 40)
+hist_bins = 40
+
+# train classifier and save svc in pickle file
+train_clf = False
+if train_clf:
+    train_classifier(folder_cars, folder_non_cars, svc_file, features_file,
+                     force_gen_feature=False, force_gen_svc=False,
+                     colorspace=color_space, orient=orient, pix_per_cell=pix_per_cell, cell_per_block=cell_per_block,
+                     hog_channel=hog_channel, spatial_size=spatial_size, hist_bins=hist_bins)
+
+# test images
+img_dir = '../test_images/'
+output_dir = '../output_images/'
+
+# get image files
+all_files = os.listdir(img_dir)
+img_files = [file_names for file_names in all_files if not file_names[0] == '.']
+#img_files = ['test1.jpg']
+out_file_name = output_dir + 'raw/marked_'
+
+# get classifier
+with open(svc_file, "rb") as f:
+    svc_data = pickle.load(f)
+svc = svc_data["svc"]
+X_scaler = svc_data["scaler"]
+
+ystarts = [400, 400, 400]
+ystops = [656, 656, 528]
+scales = [1.5, 1, 0.75]
+
+for img_file in img_files:
+    img = mpimg.imread(img_dir+img_file)
+    # find car on image
+    vehicles_boxes = []
+    for ystart, ystop, scale in zip(ystarts, ystops, scales):
+        v_boxes = find_cars(img, ystart, ystop, scale, svc, X_scaler, orient,
+                            pix_per_cell, cell_per_block, spatial_size, hist_bins)
+        if len(v_boxes) > 0:
+            for box in v_boxes:
+                vehicles_boxes.append(box)
+    draw_img = draw_boxes(img, vehicles_boxes)
+    #draw_img = generate_heatmap(img, vehicles_boxes)
+
+    cv2.imwrite(out_file_name+img_file, cv2.cvtColor(draw_img, cv2.COLOR_BGR2RGB))
+    #plt.imshow(draw_img)
+    #plt.show()
